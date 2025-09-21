af3cdf66b06424f8ffc82da43f0b007bfe9371a9
Vishad-byte <srivastavavishad0212@gmail.com>
2025-09-21 17:13:40 +0530

    package.json and index.js fixed for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
-dotenv.config({path: './.env'})
 
+dotenv.config({ path: "./.env" });
 
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
 
-connectDB()                                                 
-.then( () => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT}`)});
-        app.on("Error", (err) => {
-            console.error("Express application error:", err)
-        })
-})
-.catch( (err) => {
-    console.error("Mongo db connection failed", err);
-})
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    app.on("error", (err) => {
+      console.error("Express application error:", err);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
